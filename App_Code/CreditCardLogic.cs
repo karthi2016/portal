e3cc797ce8570c2570d165757a53a87195df33cc
membersuite <andrew@membersuite.com>
2015-03-31 15:39:02 -0400

    3.33.0.936

---
+++ App_Code/CreditCardLogic.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Script.Serialization;
+using System.Web.UI.HtmlControls;
+using MemberSuite.SDK.Results;
+using MemberSuite.SDK.Types;
+
+/// <summary>
+/// Summary description for CreditCardLogic
+/// </summary>
+public class CreditCardLogic : PortalPage
+{
+    protected string GetPriorityPaymentsConfig(string entityId)
+    {
+        if (string.IsNullOrWhiteSpace(entityId))
+            return null;
+
+        using (var api = GetServiceAPIProxy())
+        {
+            ConciergeResult<PriorityApiSettings> priorityData = api.GetPriorityConfiguration(entityId);
+            var serializer = new JavaScriptSerializer();
+            return serializer.Serialize(priorityData.ResultValue);
+        }
+    }
+}
